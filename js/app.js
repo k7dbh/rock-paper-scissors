036505e8a9e5260d835ae43f31ea3fbbf1404f56
k7dbh <203570242+k7dbh@users.noreply.github.com>
2025-06-29 11:10:21 +0300

    create playes and renders

--- js/app.js
+++ js/app.js
@@ -17,5 +17,34 @@
 
  /*------------- Functions -------------- */
 
+ function getComputerChoice() {
+    // generate a random number 0-2
+    //you can skip math.floor if you want it decimal
+    const randomIndex = Math.floor(Math.random() * choice.length)
+    // select item from the array
+    return choice[randomIndex]
+    //computerChoice = choice[randomIndex]
+ }
+ 
+
+ // initialize game state
+ function play() {
+    console.log('computerChoice',conputerChoice)
+    computerChoice = getComputerChoice()
+    console.log('computerChoice',computerChoice)
+    
+
+
+    // after updating state, render to html
+    render()
+
+ }
+ play()
+
+ // updated our UI/html directly
+ function render() {
+
+ }
+
  /*------------- Event Listeners -------------- */
 
